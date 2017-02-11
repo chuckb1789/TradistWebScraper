83f4152f8d65a9a2f51bc12bc8148049af29b770
CharlieBeck <chuckb1789@gmail.com>
2017-02-10 17:04:50 -0700

    Add text file

--- server.js
+++ server.js
@@ -2,26 +2,35 @@
 var Nightmare = require('nightmare');
 
 // create a new browser window
-var nightmare = Nightmare({ show: true });
 
-nightmare
-    .goto('https://www.mountainproject.com/v/the-bastille---n-face/105744723')
-    .click("#leftNavRoutes > tbody > tr:nth-child(1) > td:nth-child(3) > a")
-    .wait(5000)
+var protectionData = [];
+for (var i=0; i<32; i++) {
+    var nightmare = Nightmare({ show: true });
 
-    // evaluate code IN THE NIGHTMARE BROWSER
-    .evaluate(function () {
+    var scraperURL = 'https://www.mountainproject.com/v/the-bastille---n-face/105744723'
+    var scraperClick = '#leftNavRoutes > tbody > tr:nth-child('+(i+1).toString()+') > td:nth-child(3) > a'
+      nightmare
+          .goto(scraperURL)
+          .wait(8000)
+          .click(scraperClick)
+          .wait(8000)
 
-        // pass data from the nightmare browser back to this express app
-        return $('#rspCol800 > div:nth-child(15)').text();
-    })
+          // evaluate code IN THE NIGHTMARE BROWSER
+          .evaluate(function () {
 
-    // close the browser window
-    .end()
-    // the result passed into this function is the value we returned from evaluate
-    .then(function (result) {
-        console.log(result)
-    })
-    .catch(function (error) {
-        console.error('Search failed:', error);
-    });
+              // pass data from the nightmare browser back to this express app
+              return $('#rspCol800').text();
+          })
+
+          // close the browser window
+          .end()
+          // the result passed into this function is the value we returned from evaluate
+          .then(function (result) {
+              console.log(result)
+              protectionData[i] = result
+          })
+          .catch(function (error) {
+              console.error('Search failed:', error);
+          });
+  // console.log(scraperClick)
+  }
